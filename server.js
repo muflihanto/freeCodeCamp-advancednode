eb23d97f453f1628ea7096110b94309acdfc172a
muflihanto <48417028+muflihanto@users.noreply.github.com>
2023-11-10 10:49:11 +0000

    serialization of a user object

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session")
 const passport = require("passport")
 const app = express();
+const { ObjectID } = require('mongodb');
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -21,6 +22,16 @@ app.use(session({
 }));
 app.use(passport.initialize())
 app.use(passport.session())
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  // });
+});
 
 app.route("/").get((req, res) => {
   res.render("index", { title: "Hello", message: "Please log in" });
