9778ac9208a9aa21c76c5ca0649938a46afc1972
muflihanto <48417028+muflihanto@users.noreply.github.com>
2023-11-10 07:57:46 +0000

    First commit from replit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
-app.set("views", "./views/pug/");
+app.set("views", "./views/pug");
 
 app.route("/").get((req, res) => {
   res.render("index", { title: "Hello", message: "Please log in" });
