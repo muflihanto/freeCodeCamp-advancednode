5eba9619642372694cbac1d2cc57786f8182c4db
muflihanto <48417028+muflihanto@users.noreply.github.com>
2023-11-10 08:06:00 +0000

    set up passport

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
-
+const session = require("express-session")
+const passport = require("passport")
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -12,6 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
+app.use(session({
+  secret: process.env['SESSION_SECRET'],
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.route("/").get((req, res) => {
   res.render("index", { title: "Hello", message: "Please log in" });
