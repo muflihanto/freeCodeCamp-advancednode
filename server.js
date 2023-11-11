81de93bfbf0b8c37dc4e84f90eda7befed85cc61
muflihanto <48417028+muflihanto@users.noreply.github.com>
2023-11-11 07:04:58 +0000

    create new middleware

--- server.js
+++ server.js
@@ -41,6 +41,19 @@ myDB(async (client) => {
     res.redirect("/profile");
   });
 
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
+  };
+
+  app
+   .route('/profile')
+   .get(ensureAuthenticated, (req,res) => {
+      res.render('profile');
+   });
+
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
