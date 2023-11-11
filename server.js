d5bce92deb9a4be309fab807067416d0ae92f057
muflihanto <48417028+muflihanto@users.noreply.github.com>
2023-11-11 07:14:12 +0000

    logging a user out

--- server.js
+++ server.js
@@ -56,6 +56,18 @@ myDB(async (client) => {
       });
     });
 
+  app.route('/logout')
+    .get((req, res) => {
+      req.logout();
+      res.redirect('/');
+    });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
