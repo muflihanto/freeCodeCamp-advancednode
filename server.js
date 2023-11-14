59a26c605572bd22eb3d6305a1d2503e2929c8f9
muflihanto <48417028+muflihanto@users.noreply.github.com>
2023-11-14 08:07:38 +0000

    authentication with socket io

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const routes = require('./routes.js');
 const auth = require('./auth.js');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env["MONGO_URI"];
+const store = new MongoStore({ url: URI });
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require("cookie-parser")
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -20,6 +25,7 @@ app.set("views", "./views/pug");
 app.use(
   session({
     secret: process.env["SESSION_SECRET"],
+    key: 'express.sid',
     resave: true,
     saveUninitialized: true,
     cookie: { secure: false },
@@ -33,11 +39,36 @@ myDB(async (client) => {
   routes(app, myDataBase)
   auth(app, myDataBase)
 
+  function onAuthorizeSuccess(data, accept) {
+    console.log('successful connection to socket.io');
+
+    accept(null, true);
+  }
+
+  function onAuthorizeFail(data, message, error, accept) {
+    if (error) throw new Error(message);
+    console.log('failed connection to socket.io:', message);
+    accept(null, false);
+  }
+
+
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: 'express.sid',
+      secret: process.env["SESSION_SECRET"],
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAuthorizeFail
+    })
+  );
+
   let currentUsers = 0;
   io.on('connection', socket => {
     ++currentUsers;
     io.emit('user count', currentUsers);
     console.log('A user has connected');
+    console.log('user ' + socket.request.user.username + ' connected');
     socket.on('disconnect', () => {
       --currentUsers;
       io.emit('user count', currentUsers);
